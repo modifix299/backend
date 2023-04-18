3ea20f278cd9185fda493d4afdf96f662db67787
achchuthan-suntharalingam <achchu0103@gmail.com>
2023-04-18 10:35:58 +0530

    auth

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/dbConnection');
 const mongoose = require('mongoose');
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 8000
 
 
-
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 app.use(errorHandler)
@@ -16,12 +18,13 @@ app.use(errorHandler)
 //Routes
 app.use('/users', require('./routes/userRoutes'));
 app.use('/products', require('./routes/productRoutes'));
+app.use('/admin/auth', require('./routes/adminAuthRoutes.js'));
  
 connectDB().then(()=>{
     console.log('Connected to MongoDB');
-  });
+});
 
 app.listen(PORT,() => {
     console.log(`Server Running On Port ${PORT} `);
-  });
+});
 
